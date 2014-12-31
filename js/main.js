48a2e505325956fdac36b9f3c8fa3b946604bfca
trevor <trevor@mediadrink.com>
2014-12-31 15:40:46 -0800

    back to ajax thanks to stack overflow
    window.instgrm.Embeds.process()

--- js/main.js
+++ js/main.js
@@ -30,12 +30,18 @@ function Person(initial,total   ,pic,bubble){
   pic.on("click",this,function(e){
     var self = e.data
     self.talk();
-    self.bubble.parent().attr("href", instaFrame[self.initial + self.counter()]);
+    self.bubble.parent()
+      .attr("href", instaLink[self.initial + self.counter()])
+      .attr("target","blank");
+
+    $("#instaExtra").append(getInsta(instaLink[self.initial + self.counter()]));
   });
 
-  bubble.parent().magnificPopup({
-    type: 'iframe'
-  });
+
+   //
+  //  bubble.parent().magnificPopup({
+  //    type: 'iframe'
+  //  });
 };
 
 jQuery(document).ready(function(){
@@ -48,13 +54,30 @@ jQuery(document).ready(function(){
     $("#r-talk img:hidden").attr("src","img/R2.png").show("slow");
   },2000);
 
+  window.getInsta = function(page){
+    var proxy = "proxy.php?requrl=";
+    var api = "http://api.instagram.com/oembed?url=";
+    var ajax = $.getJSON(proxy + api + page,function(data){
+      console.log(data)
+      });
+  }
+
 });
 
-
-var instaFrame = {
-  "L1" : "http://instagram.com/p/dnQi4EGuZx/embed/",
+var instaLink = {
+	"R1" : "http://instagram.com/p/wwlaUPGCH4/",
+	"R6" : "http://instagram.com/p/kdRQ_vGCKf/",
+	"R7" : "http://instagram.com/p/wzHGbfDI3d/",
+	"R8" : "http://instagram.com/p/mHLIcaGCPl/",
+	"R9" : "http://instagram.com/p/w-wp_HGCFt/",
+	"R10": "http://instagram.com/p/mosocPNnH5/",
+	"T3" : "http://instagram.com/p/mQJ26fmCHh/",
+	"T5" : "http://instagram.com/p/i0WQlamCPB/",
+	"T9" : "http://instagram.com/p/eS0YWDNnFr/",
+	"T10": "http://instagram.com/p/vvtnSomCEj/",
+	"L1" : "http://instagram.com/p/vPuFrYGCLB/",
+	"L2" : "http://instagram.com/p/sYKL8lmCG3/",
+	"L5" : "http://instagram.com/p/trZsyqGCFa/",
+	"L6" : "http://instagram.com/p/ty7eNpmCGS/",
+	"L7" : "http://instagram.com/p/v9hmJwmCFo/"
 }
-
-var lisaLinks = [];
-var rhinoLinks = [];
-var trevorLinks = [];
--- proxy.php
+++ proxy.php
@@ -1,6 +1,5 @@
 <?php
 $file = file_get_contents($_GET['requrl']);
 $json = json_decode($file);
-
 echo $json->html;
 ?>
